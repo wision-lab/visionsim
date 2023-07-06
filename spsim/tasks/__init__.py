d190e88e462f1a1832f5f7e96300038ba6320fba
Sacha Jungerman <jungerm2@illinois.edu>
2023-07-06 14:10:49 -0500

    add max-threads config option

--- spsim/tasks/__init__.py
+++ spsim/tasks/__init__.py
@@ -1,8 +1,25 @@
+import os
 from importlib import metadata
 
-from invoke import Collection, Program
+from invoke import Collection, Config, Program
 
 from . import blender, colmap, dataset, emulate, ffmpeg, interpolate, transforms
+
+
+class SPSIMConfig(Config):
+    """Override `prefix` attribute enabling config options like:
+
+    Environment variables:
+        SPSIM_MAX_THREADS instead of INVOKE_MAX_THREADS
+
+    Configuration files:
+        spsim.(yaml, yml, json or py) instead of invoke.xxx
+
+    For more see: https://docs.pyinvoke.org/en/latest/concepts/configuration.html
+    """
+
+    prefix = "spsim"
+
 
 ns = Collection()
 ns.add_collection(Collection.from_module(colmap))
@@ -13,7 +30,10 @@ ns.add_collection(Collection.from_module(interpolate))
 ns.add_collection(Collection.from_module(blender))
 ns.add_collection(Collection.from_module(transforms))
 
+ns.configure({"max_threads": os.cpu_count()})
+
+
 # Note: This is the version of the installed pkg, not the imported one.
 #   They can only differ if pkg is installed with -e option.
 version = metadata.version("spsim")
-program = Program(name="spsim", version=str(version), namespace=ns)
+program = Program(name="spsim", version=str(version), namespace=ns, config_class=SPSIMConfig)
