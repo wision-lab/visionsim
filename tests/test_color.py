9f61a7c10a82ee1ac475081167aa1b8775930b8a
Sacha Jungerman <jungerm2@illinois.edu>
2023-11-14 12:54:50 -0600

    add tonemapping tests

---
+++ tests/test_color.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from spsim.color import linearrgb_to_srgb, srgb_to_linearrgb
+
+
+def test_linearrgb_to_srgb_and_back():
+    img = np.random.random((100, 100))
+    round_trip_img = srgb_to_linearrgb(linearrgb_to_srgb(img))
+    assert np.allclose(img, round_trip_img)
+
+
+def test_srgb_to_linearrgb_and_back():
+    img = np.random.random((100, 100))
+    round_trip_img = linearrgb_to_srgb(srgb_to_linearrgb(img))
+    assert np.allclose(img, round_trip_img)
