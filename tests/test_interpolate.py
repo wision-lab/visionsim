eb73e2e139019b3cbdfe3f0e51c5b134bce1bf69
Max Leblang <max@leblang.com>
2024-11-24 23:38:52 -0500

    Try to fix paths

--- tests/test_interpolate.py
+++ tests/test_interpolate.py
@@ -1,21 +1,23 @@
 import os
 import json
 import numpy as np
+from pathlib import Path
+from scipy.spatial.transform import Rotation as R
 from spsim.interpolate import interpolate_frames, interpolate_poses
 from spsim.schema import IMG_SCHEMA, _read_and_validate
 from PIL import Image
 
 
 def test_interpolate_poses():
-    test_transforms_path = "tests/test_files/interpolate_transforms/"
+    test_transforms_path = Path("test_files/interpolate_transforms/")
 
     # Load in initial transforms file
-    transforms = _read_and_validate(path=test_transforms_path+"transforms.json", schema=IMG_SCHEMA)
+    transforms = _read_and_validate(path=test_transforms_path / "transforms.json", schema=IMG_SCHEMA)
     # Interpolate the poses
     new_poses = interpolate_poses(transforms)
 
     # Extract matrices from expected transform file
-    with open(test_transforms_path + "expected_transforms.json", "r") as f:
+    with open(test_transforms_path / "expected_transforms.json", "r") as f:
         transforms = json.load(f)
 
     expected_poses = list()
@@ -29,6 +31,42 @@ def test_interpolate_poses():
         interpolated_pose = np.array(interpolated_pose)
 
         assert np.allclose(expected_pose, interpolated_pose)
+
+
+def test_interplate_rotation_matrix():
+    # Pose at origin with no rotation
+    start_pose = np.array([
+        [1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,0],
+        [0,0,0,1]
+    ])
+    # Pose at (1,1,1) with 45 degree rotation
+    end_pose = np.array([
+        [45,0,0,1],
+        [0,45,0,1],
+        [0,0,45,1],
+        [0,0,0,1]
+    ])
+    end_pose = end_pose/np.linalg.det(end_pose)
+    # Put test matrices in transforms format
+    transforms = {
+        "frames": [
+            {
+                "file_path": "0",
+                "transform_matrix": start_pose
+            },
+            {
+                "file_path": "1",
+                "transform_matrix": end_pose
+            }
+        ]
+    }
+
+    # Interpolate the poses
+    new_poses = interpolate_poses(transforms)
+    print(new_poses)
+
 
 
 def test_interpolate_frames(tmp_path):
