06634c0063c5015a5b26856dd46284b7354d52a6
Sacha Jungerman <jungerm2@illinois.edu>
2025-03-25 18:01:00 -0500

    use RotationSpline to enable extrapolation and derivatives

--- spsim/interpolate/pose.py
+++ spsim/interpolate/pose.py
@@ -3,14 +3,26 @@ from __future__ import annotations
 import numpy as np
 import numpy.typing as npt
 from scipy.interpolate import make_interp_spline
-from scipy.spatial.transform import Rotation, Slerp
+from scipy.spatial.transform import Rotation, RotationSpline
+from typing_extensions import Literal, cast
 
 
 class pose_interp:
-    """Linearly interpolate between 4x4 transformation matrices by interpolating it's components:
-    Slerp the rotations and lerp the translations. This cannot do extrapolation (yet)."""
+    """Linearly interpolate between 4x4 (or 3x4) transformation matrices by interpolating it's components"""
 
     def __init__(self, transforms: npt.ArrayLike, ts: npt.ArrayLike | None = None, k=1, normalize=False) -> None:
+        """Create a spline from the rotational and translational components of each pose.
+        Specifically, we use scipy's RotationSpline and BSpline respectively.
+
+        Args:
+            transforms (npt.ArrayLike): Poses to interpolate as matrices.
+            ts (npt.ArrayLike | None, optional): Time of each pose. Defaults to None, meaning linspace between [0, 1].
+            k (int, optional): B-spline degree for translation. Default is linear (i.e: 1).
+            normalize (bool, optional): If true, normalize rotations by their determinants. Defaults to False.
+
+        Raises:
+            RuntimeError: raised when rotations are not normalized.
+        """
         self.transforms = np.array(transforms)
         self.ts = np.linspace(0, 1, len(self.transforms)) if ts is None else np.array(ts)
         self.determinants = np.linalg.det(self.transforms[:, :3, :3])
@@ -22,18 +34,43 @@ class pose_interp:
                 "Rotation matrices in poses must have determinant of 1. You may also try setting normalize to True."
             )
 
-        self._rotation_interp = Slerp(self.ts, Rotation.from_matrix(self.transforms[:, :3, :3]))
+        self._rotation_interp = RotationSpline(self.ts, Rotation.from_matrix(self.transforms[:, :3, :3]))
         self._translation_interp = make_interp_spline(self.ts, self.transforms[:, :3, -1], k=k, axis=0)
 
-    def __call__(self, t: npt.ArrayLike) -> npt.NDArray:
-        t_shape = np.shape(t)
-        t = np.atleast_1d(t)
+    def __call__(self, ts: npt.ArrayLike, order: Literal[0, 1, 2] = 0) -> npt.NDArray:
+        """Compute interpolated poses, or their derivatives.
 
-        bottom = np.array([0.0, 0.0, 0.0, 1.0])
-        bottom = np.tile(bottom, (*t.shape, 1, 1))
+        Args:
+            ts (npt.ArrayLike): Times of interest.
+            order (Literal[0, 1, 2], optional): Order of differentiation:
 
-        R = self._rotation_interp(t).as_matrix()
-        t = self._translation_interp(t)[..., None]
-        Rt = np.concatenate([R, t], axis=2)
-        transforms = np.concatenate([Rt, bottom], axis=1)
-        return transforms.reshape(*t_shape, 4, 4)
+                * 0 (default): return pose as 4x4 matrices
+                * 1: return velocities as 2x3 matrices, where the first row is the angular rates
+                    in rad/sec and second row are positional velocities.
+                * 2: return the accelerations as 2x3 matrices, packaged as angular acceleration
+                    (in rad/sec/sec) the positional.
+            Defaults to 0.
+
+        Returns:
+            npt.NDArray: Interpolated poses (Tx4x4) or their derivatives (Tx2x3)
+
+        :meta public:
+        """
+        if order not in [0, 1, 2]:
+            raise ValueError("`order` must be 0, 1 or 2.")
+
+        t_shape = np.shape(ts)
+        ts = np.atleast_1d(ts)
+
+        R = self._rotation_interp(ts, order)
+        t = self._translation_interp(ts, order)
+
+        if order == 0:
+            bottom = np.array([0.0, 0.0, 0.0, 1.0])
+            bottom = np.tile(bottom, (*ts.shape, 1, 1))
+
+            # Cast to make mypy happy
+            Rt = np.concatenate([cast(Rotation, R).as_matrix(), t[..., None]], axis=2)
+            transforms = np.concatenate([Rt, bottom], axis=1)
+            return transforms.reshape(*t_shape, 4, 4)
+        return np.concatenate([R, t], axis=0)
