76636ecd47ca09a83deb68f0c3c971b2593ef250
Shantanu Gupta <sgupta@cs.wisc.edu>
2025-09-26 16:00:02 -0500

    Bayer pattern and demosaicing, skimage dependency addition

--- visionsim/utils/color.py
+++ visionsim/utils/color.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 import numpy as np
 import numpy.typing as npt
 import torch
-from typing_extensions import overload
+from scipy.ndimage import correlate
+from skimage.util import img_as_float
+from typing_extensions import Literal, overload
 
 
 @overload
@@ -56,3 +58,148 @@ def linearrgb_to_srgb(img: torch.Tensor | npt.NDArray) -> torch.Tensor | npt.NDA
     img[mask] = img[mask] * 12.92  # type: ignore
     img[~mask] = module.clip(1.055 * img[~mask] ** (1.0 / 2.4) - 0.055, 0.0, 1.0)
     return img
+
+
+def rgb2raw_bayer(rgb: torch.Tensor | npt.NDArray,
+                  cfa_pattern: Literal["rggb"] = "rggb"
+) -> torch.Tensor | npt.NDArray:
+    """Mosaicing
+
+    Realizes a mosaiced CFA image as would be sampled by a real Bayer-patterned sensor
+
+    Args:
+        rgb: hypothetical true RGB signal
+        cfa_pattern: Bayer pattern (only RGGB implemented for now)
+
+    Returns:
+        raw: CFA mosaiced data
+    """
+    if cfa_pattern == "rggb":
+        raw = np.copy(rgb[:,:,0])
+        raw[::2, 1::2] = rgb[::2, 1::2, 1]
+        raw[1::2, ::2] = rgb[1::2, ::2, 1]
+        raw[1::2, 1::2] = rgb[1::2, 1::2, 2]
+    else:
+        raise NotImplementedError
+    return raw
+
+
+def raw2rgb_bayer(raw: torch.Tensor | npt.NDArray,
+                  cfa_pattern: Literal["rggb"] = "rggb",
+                  method: Literal["off", "bilinear", "MHC04"] = "bilinear"
+) -> torch.Tensor | npt.NDArray:
+    """Demosaicing
+
+    Convolution-based implementation as suggested by Malvar et al., 
+    "High-quality linear interpolation for demosaicing of Bayer-patterned color images", ICASSP 2004.
+    https://home.cis.rit.edu/~cnspci/references/dip/demosaicking/malvar2004.pdf
+
+    Alternative implementations are also available from OpenCV:
+        rgb = cv2.cvtColor(<uint16_array>, cv2.COLOR_BAYER_BG2BGR)[:,:,::-1],
+    and the colour-demosaicing library (https://pypi.org/project/colour-demosaicing):
+        rgb = demosaicing_CFA_Bayer_bilinear(raw, pattern="RGGB")
+        rgb = demosaicing_CFA_Bayer_Malvar2004(raw, pattern="RGGB"),
+    which appear to give similar results but could run faster (not benchmarked).
+
+    Args:
+        raw: input array (has to be exactly 2D)
+        cfa_pattern: Bayer pattern shape (only RGGB implemented for now)
+
+    Returns:
+        rgb: demosaiced RGB image
+    """
+
+    if cfa_pattern == "rggb":
+        if any(((L % 2) != 0) for L in raw.shape):
+            raise RuntimeError(f"Expected even-length raw array, got {(raw.shape)}")
+
+        if not isinstance(raw, np.floating):
+            raw = img_as_float(raw)
+
+        R, G1, G2, B = raw[::2, ::2], raw[::2, 1::2], raw[1::2, ::2], raw[1::2, 1::2]
+
+        rgb = np.zeros(raw.shape + (3,), dtype=raw.dtype)
+        rgb[::2, ::2, 0] = R
+        rgb[::2, 1::2, 1] = G1
+        rgb[1::2, ::2, 1] = G2
+        rgb[1::2, 1::2, 2] = B
+
+        if method == "off":
+            return rgb
+
+        if method in ["bilinear", "MHC04"]:
+            h_avg_fw_x = np.array([[0., 0.5, 0.5]])
+            h_avg_bw_x = np.fliplr(h_avg_fw_x)
+            
+            rgb[::2, ::2, 1] = 0.5 * (correlate(G1, h_avg_bw_x, mode="nearest")
+                                      + correlate(G2, h_avg_bw_x.T, mode="nearest"))
+            rgb[::2, ::2, 2] = correlate(correlate(B, h_avg_bw_x, mode="nearest"),
+                                          h_avg_bw_x.T, mode="nearest")
+
+            rgb[::2, 1::2, 0] = correlate(R, h_avg_fw_x, mode="nearest")
+            rgb[::2, 1::2, 2] = correlate(B, h_avg_bw_x.T, mode="nearest")
+            rgb[1::2, ::2, 0] = correlate(R, h_avg_fw_x.T, mode="nearest")
+            rgb[1::2, ::2, 2] = correlate(B, h_avg_bw_x, mode="nearest")
+
+            rgb[1::2, 1::2, 0] = correlate(correlate(R, h_avg_fw_x, mode="nearest"),
+                                            h_avg_fw_x.T, mode="nearest")
+            rgb[1::2, 1::2, 1] = 0.5 * (correlate(G1, h_avg_fw_x.T, mode="nearest")
+                                        + correlate(G2, h_avg_fw_x, mode="nearest"))
+
+            if method == "bilinear":
+                return rgb
+
+            if method == "MHC04":
+                # Kernels in Fig. 2 of Malvar et al., along with weight factors for combination.
+                # This implementation is a little idiosyncratic because it realizes the 2D convolution
+                # via a sequence of 1D convolutions, probably not worth it in hindsight...
+                h_avg_nhood_x = np.array([[0.5, 0., 0.5]])
+
+                av_R = correlate(correlate(R, h_avg_nhood_x, mode="nearest"),
+                                  h_avg_nhood_x.T, mode="nearest")
+                diff_R = R - av_R
+
+                av_B = correlate(correlate(B, h_avg_nhood_x, mode="nearest"),
+                                  h_avg_nhood_x.T, mode="nearest")
+                diff_B = B - av_B
+
+                rgb[::2, ::2, 1] += (1/2) * diff_R
+                rgb[::2, ::2, 2] += (3/4) * diff_B
+                rgb[1::2, 1::2, 1] += (1/2) * diff_B
+                rgb[1::2, 1::2, 0] += (3/4) * diff_R
+                
+                av_G2_at_G1 = correlate(correlate(G2, h_avg_fw_x, mode="nearest"),
+                                         h_avg_bw_x.T, mode="nearest")
+
+                av_G1_at_G1_R = (correlate(G1, h_avg_nhood_x, mode="nearest")
+                                 - correlate(G1, 0.5*h_avg_nhood_x.T, mode="nearest"))
+                av_G1_R = (1/5) * (2*av_G1_at_G1_R + 4*av_G2_at_G1)
+                diff_G1_R = G1 - av_G1_R
+                rgb[::2, 1::2, 0] += (5/8) * diff_G1_R
+
+                av_G1_at_G1_B = (correlate(G1, h_avg_nhood_x.T, mode="nearest")
+                                 - correlate(G1, 0.5*h_avg_nhood_x, mode="nearest"))
+                av_G1_B = (1/5) * (2*av_G1_at_G1_B + 4*av_G2_at_G1)
+                diff_G1_B = G1 - av_G1_B
+                rgb[::2, 1::2, 2] += (5/8) * diff_G1_B
+
+                av_G1_at_G2 = correlate(correlate(G1, h_avg_bw_x, mode="nearest"),
+                                         h_avg_fw_x.T, mode="nearest")
+
+                av_G2_at_G2_R = (correlate(G2, h_avg_nhood_x.T, mode="nearest")
+                                 - correlate(G2, 0.5*h_avg_nhood_x, mode="nearest"))
+                av_G2_R = (1/5) * (4*av_G1_at_G2 + 2*av_G2_at_G2_R)
+                diff_G2_R = G2 - av_G2_R
+                rgb[1::2, ::2, 0] += (5/8) * diff_G2_R
+
+                av_G2_at_G2_B = (correlate(G2, h_avg_nhood_x, mode="nearest")
+                                 - correlate(G2, 0.5*h_avg_nhood_x.T, mode="nearest"))
+                av_G2_B = (1/5) * (4*av_G1_at_G2 + 2*av_G2_at_G2_B)
+                diff_G2_B = G2 - av_G2_B
+                rgb[1::2, ::2, 2] += (5/8) * diff_G2_B
+
+                return rgb
+    else:
+        raise NotImplementedError
+    return rgb
+
