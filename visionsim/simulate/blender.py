ac03e032bb47555b6ce34af30428d97bd743253e
Sacha Jungerman <jungerm2@illinois.edu>
2026-01-13 08:57:06 -0600

    only terminate on windows

--- visionsim/simulate/blender.py
+++ visionsim/simulate/blender.py
@@ -291,12 +291,10 @@ class BlenderServer(rpyc.utils.server.Server):
 
             finally:
                 for p in procs:
-                    # We need to send two CTRL+C events to blender to kill it, but only one on windows
+                    # We need to send two CTRL+C events to blender to kill it, but not on windows
                     if platform.system() != "Windows":
                         p.send_signal(signal.SIGINT)
                         p.send_signal(signal.SIGINT)
-                    else:
-                        p.send_signal(signal.CTRL_C_EVENT)
 
                 for p in procs:
                     # Ensure process is killed if CTRL+C failed
