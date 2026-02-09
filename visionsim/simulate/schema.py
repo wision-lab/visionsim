9e88bd50fd8e345313f29903278b0f6fce3f08f8
Sacha Jungerman <jungerm2@illinois.edu>
2026-02-09 15:36:15 -0600

    add docstrings

--- visionsim/simulate/schema.py
+++ visionsim/simulate/schema.py
@@ -27,12 +27,13 @@ DEFAULT_PRAGMAS = {
 }
 
 
-class BaseModel(Model):
+class _BaseModel(Model):
     class Meta:
         model_metadata_class = ThreadSafeDatabaseMetadata
 
 
-class Camera(BaseModel):
+class Camera(_BaseModel):
+    """A database model that mirrors :class:`models.Camera <visionsim.dataset.models.Camera>` with added blender-specific fields."""
     angle = FloatField(null=True)
     angle_x = FloatField(null=True)
     angle_y = FloatField(null=True)
@@ -63,31 +64,56 @@ class Camera(BaseModel):
     cy = FloatField()
 
 
-class Data(BaseModel):
+class Data(_BaseModel):
+    """A database model that mirrors :class:`models.Data <visionsim.dataset.models.Data>`."""
     path = TextField()
     bitpack_dim = IntegerField(null=True)
 
 
-class Frame(BaseModel):
+class Frame(_BaseModel):
+    """A database model that mirrors :class:`models.Frame <visionsim.dataset.models.Frame>`."""
     data = ForeignKeyField(Data, backref="frames", on_delete="CASCADE", index=True)
     camera = ForeignKeyField(Camera, backref="frames", on_delete="CASCADE", index=True)
     transform_matrix = JSONField()
     offset = IntegerField(null=True)
 
 
-MODELS: tuple[type[BaseModel], ...] = (Camera, Data, Frame)
+MODELS: tuple[type[_BaseModel], ...] = (Camera, Data, Frame)
 
 
 class Metadata:
     def __init__(self, path: str | os.PathLike):
+        """Initialize a metadata instance.
+
+        Args:
+            path (str | os.PathLike): Metadata dataset path
+        """
         self.path = Path(path).resolve()
 
     @classmethod
     def load(cls, path: str | os.PathLike) -> Self:
+        """Same as :meth:`__init__`, added to better mirror :class:`models.Metadata <visionsim.dataset.models.Metadata>`"""
         return cls(path)
 
     @classmethod
     def from_dense_transforms(cls, path: str | os.PathLike, transforms: Iterator[dict[str, Any]]) -> Self:
+        """Instantiate a new dataset from the provided dense transforms, persisting all data to disk.
+
+        Warning:
+            This will overwrite any pre-existing dataset found at ``path``.
+        
+        Warning:
+            This method can be slow, as it inserts rows into the database on at a time which is suboptimal. 
+            However, it ensures correctness as camera and data rows are checked for uniqueness.
+
+        Args:
+            path (str | os.PathLike): Metadata dataset path
+            transforms (Iterator[dict[str, Any]]): Dictionary containing frame and camera data. Keys are filtered 
+                before inserting rows, so only keys that correspond to valid columns will be saved.  
+
+        Returns:
+            Self: new metadata database object, with all data persisting on disk. 
+        """
         if Path(path).exists():
             Path(path).unlink()
 
@@ -114,6 +140,15 @@ class Metadata:
     def iter_dense_transforms(
         self, as_data_type: str = "file_path", exclude_none: bool = True
     ) -> Iterator[dict[str, Any]]:
+        """Yield dictionaries containing all frame and camera information, one per frame.
+
+        Args:
+            as_data_type (str, optional): What to rename "path" key to. Defaults to "file_path".
+            exclude_none (bool, optional): If true, exclude columns that have a null value. Defaults to True.
+
+        Yields:
+            Iterator[dict[str, Any]]: Dictionaries containing all relevant frame data
+        """
         db = SqliteExtDatabase(self.path, pragmas=DEFAULT_PRAGMAS)
         with db.connection_context():
             with db.bind_ctx(MODELS):
@@ -132,10 +167,12 @@ class Metadata:
                     yield transform
 
     def to_dense_transforms(self, *args, **kwargs) -> list[dict[str, Any]]:
+        """Same as :meth:`iter_dense_transforms` but returns a list instead of a generator."""
         return list(self.iter_dense_transforms(*args, **kwargs))
 
     @cached_property
     def cameras(self) -> list[dict[str, Any]]:
+        """List of defined cameras."""
         db = SqliteExtDatabase(self.path, pragmas=DEFAULT_PRAGMAS)
         with db.connection_context():
             with db.bind_ctx(MODELS):
