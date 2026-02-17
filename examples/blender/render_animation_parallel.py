90f51ccd924ea5d1ac0f526bfc2eb8a136247afe
Sacha Jungerman <jungerm2@illinois.edu>
2026-02-17 12:13:39 -0600

    revert to default process start method, add main guards instead

--- examples/blender/render_animation_parallel.py
+++ examples/blender/render_animation_parallel.py
@@ -5,8 +5,9 @@ from rich.progress import Progress
 
 from visionsim.simulate.blender import BlenderClients
 
-with BlenderClients.spawn(jobs=2, timeout=30, log="logs") as clients, Progress() as progress:
-    clients.initialize(Path("assets/monkey.blend").resolve(), Path("renders/monkey").resolve())
-    clients.include_frames()
-    task = progress.add_task("Rendering monkey.blend...")
-    clients.render_animation(update_fn=partial(progress.update, task))
+if __name__ == "__main__":
+    with BlenderClients.spawn(jobs=2, timeout=30, log="logs") as clients, Progress() as progress:
+        clients.initialize(Path("assets/monkey.blend").resolve(), Path("renders/monkey").resolve())
+        clients.include_frames()
+        task = progress.add_task("Rendering monkey.blend...")
+        clients.render_animation(update_fn=partial(progress.update, task))
